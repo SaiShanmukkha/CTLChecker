e17895631094a1e3ef1ef879ce0d5d484b17d3a3
Venkata Praveen Kumar Sana <Venkatapraveen008@gmail.com>
2024-12-03 19:59:05 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@
  * This generated file contains a sample Java application project to get you started.
  * For more details on building Java & JVM projects, please refer to https://docs.gradle.org/8.11.1/userguide/building_java_projects.html in the Gradle documentation.
  */
-
+ 
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
