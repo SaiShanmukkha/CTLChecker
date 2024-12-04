e98971baa31956c4bd083a8123b875668ab384c2
Venkata Praveen Kumar Sana <Venkatapraveen008@gmail.com>
2024-12-03 19:58:54 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
-
+ 
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
